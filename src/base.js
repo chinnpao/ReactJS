b475388fb2bcccee9f0147e03df28d71f8aea61f
chinnpao <chinnpaotaing@gmail.com>
2017-10-09 15:38:22 +0700

    done

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from "re-base";
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyA649pvOQGAm965yofCIPBN6u5ZooGUW80",
+  authDomain: "catch-of-the-day-1578e.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-1578e.firebaseio.com"
+});
+
+export default base;
---
+++ src/components/StorePicker.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { getFunName } from "../helpers";
+// import $ from 'jquery'; 
+
+class StorePicker extends React.Component {
+	// constructor() {
+	// 	super();
+	// 	this.goToStore = this.goToStore.bind(this);
+	// }
+	goToStore(e) {
+		e.preventDefault();
+		const storeId = this.storeInput.value;
+		console.log(`Going to ${storeId}`);
+
+		this.context.router.transitionTo(`/store/${storeId}`);
+
+	}
+
+	render() {
+		return (
+			<form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
+				<h2>Please enter a store</h2>
+				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
+				<button type="submit">Visit Store</button>
+			</form>
+		)
+	}
+
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
+}
+
+export default StorePicker;
